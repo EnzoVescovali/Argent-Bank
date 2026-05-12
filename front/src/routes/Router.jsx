4d319daf7f215fc978f81f5eddac01379bc09336
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-05-12 19:07:28 +0200

    feat: add React routes setup

---
+++ front/src/routes/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Profile from '../pages/Profile'
+
+
+function Router() {
+  return (
+    <>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    </>
+  )
+}
+
+export default Router
