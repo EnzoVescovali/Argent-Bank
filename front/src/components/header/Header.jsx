b97fe04a3e23e6d160f5f4b0706c7b90da459120
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-07-16 22:21:06 +0200

    feat: add hero background image

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import bankTree from '../../assets/bank-tree.jpeg'
 
 function Header() {
   return (
-    <>
-      <div className="hero">
-        <section className="hero-content">
-          <h2 className="sr-only">Promoted Content</h2>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          <p className="text">Open a savings account with Argent Bank today!</p>
-        </section>
-      </div>
-    </>
+    <div className="hero" style={{ backgroundImage: `url(${bankTree})` }}>
+      <section className="hero-content">
+        <h2 className="sr-only">Promoted Content</h2>
+        <p className="subtitle">No fees.</p>
+        <p className="subtitle">No minimum deposit.</p>
+        <p className="subtitle">High interest rates.</p>
+        <p className="text">Open a savings account with Argent Bank today!</p>
+      </section>
+    </div>
   )
 }
 
